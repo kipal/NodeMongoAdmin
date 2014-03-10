9d4f76c74fefde977d47789d7431a497e4e66c41
kipal42 <kipal42@gmail.com>
2014-03-10 22:48:24 +0100

    use the new changes in contour-fw

--- config/server-config.js
+++ config/server-config.js
@@ -1,6 +1,6 @@
 module.exports = {
         'frontends' : {
-            'public' : new Contour.Core.Server(3000)
+            'web' : new Contour.Core.Server(3000)
         },
         'apis' : {
             'db' : new Contour.Core.DB.MongoDB.Server(8080)
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 Contour = require('contour-fw');
 var serverConfig = require(__dirname + '/config/server-config.js');
 
+serverConfig.frontends.web.setIsCurrent(true);
+
 Contour.Core.Bootstrap.setConfig({
     basicPath : __dirname,
     servers   : serverConfig
 });
+
+Contour.Core.Bootstrap.run();
