c1e6439e9461f3be63db64422612506b5a5fdf75
kipal42 <kipal42@gmail.com>
2014-03-30 17:34:34 +0200

    Service register on.

--- config/server-config.js
+++ config/server-config.js
@@ -1,6 +1,8 @@
+Register = Contour.Core.ClientScript.Register(new Contour.Core.ClientScript.Parser());
+
 module.exports = {
     frontend : {
-        web : new Contour.Core.Frontend.Server(3000, Service.Core.ClientScript.Register)
+        web : new Contour.Core.Frontend.Server(3000, new Contour.Core.Frontend.ResponseHandler(new Register()))
     },
     api : {
         mongoDB : new Contour.Core.DB.MongoDB.Server(8080)
